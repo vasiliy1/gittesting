8576028487cf49003d6356336a29a22b951bba91
vasiliy <zhar.vasiliy@gmail.com>
2016-12-30 13:20:36 +0200

    made spring boot application

---
+++ src/main/java/ua/hypson/testing/git/Application.java
@@ -0,0 +1,14 @@
+package ua.hypson.testing.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vasiliy Zhar
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
